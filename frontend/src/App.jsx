0484d5c90e0d36e9722fc18730cf88398d907d5b
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-01-21 15:41:09 -0800

    added default styling/background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import Nav from './components/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
