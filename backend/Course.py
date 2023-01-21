ec67fdbc87ee7ac0c4232b79dfd401f23147eebc
Minh Au <auduongminh2004@gmail.com>
2023-01-21 13:25:25 -0800

    add fields and stubs

--- backend/Course.py
+++ backend/Course.py
@@ -1,5 +1,20 @@
 from Section import *
 
 class Course:
+    sections = {} # {"name": Section}
+    creditsNum = 0
+
     def __init__(self) -> None:
+        pass
+
+    def addSection(self, section: Section) -> None:
+        """Add a (lecture) section the list of Sections"""
+        pass
+
+    def getSections(self) -> list[Section]:
+        """return a list of all Sections offered for this course"""
+        pass 
+    
+    def getCredits(self) -> None:
+        """return the credits of the course"""
         pass
