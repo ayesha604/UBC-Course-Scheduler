cdc6b5f6604b9db0a365cecc42886c51de38e776
Minh Au <auduongminh2004@gmail.com>
2023-01-21 14:07:39 -0800

    implement constructor and simple get functions

--- backend/Course.py
+++ backend/Course.py
@@ -5,22 +5,21 @@ class Course:
     sections = [] # [Section]
     creditsNum = 0
 
-    def __init__(self, courseName: str) -> None:
+    def __init__(self, courseName: str, sections: list[str], creditsNum: int) -> None:
         """Create a course object"""
-        pass
-
-    def addSection(self, section: Section) -> None:
-        """Add a (lecture) section the list of Sections"""
-        pass
+        self.courseName = courseName
+        self.sections = sections
+        self.creditsNum = creditsNum
+        return
 
     def getSections(self) -> list[Section]:
         """return a list of all Sections offered for this course"""
-        pass 
+        return self.sections.copy()
     
     def getCredits(self) -> int:
         """return the credits of the course"""
-        pass
+        return self.creditsNum
 
     def getCourseName(self) -> str:
         """return the course name"""
-        pass
+        return self.courseName
