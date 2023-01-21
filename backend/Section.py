e0791c818bc0be0948e0975d3f9816f5be579861
Minh Au <auduongminh2004@gmail.com>
2023-01-21 13:39:46 -0800

    add fields and stubs

--- backend/Section.py
+++ backend/Section.py
@@ -1,3 +1,36 @@
 class Section:
+    sectionName = ""
+    day = ""
+    startTime = 0
+    endTime = 0
+    term = 1 # 1 or 2
+    location = ""
+    professor = ""
+    dependencies = [] # [Section]
+
     def __init__(self) -> None:
+        pass
+
+    def getSectionName(self) -> str:
+        """return section name"""
+        pass
+
+    def getTime(self) -> dict[str: tuple(int, int)]:
+        """Return a dict with day as key and (start, end) as value""" 
+        pass
+
+    def getTerm(self) -> int:
+        """return the term the section is offered in"""
+        pass
+
+    def getLocation(self) -> str:
+        """return the building the section is offered in"""
+        pass
+
+    def getProf(self) -> str:
+        """return the instructor's name"""
+        pass
+
+    def getDependencies(self) -> list[str]:
+        """return a list of dependencies (e.g., labs, tutorials, etc.)"""
         pass
