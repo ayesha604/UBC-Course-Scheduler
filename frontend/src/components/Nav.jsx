e5c68a8d8cc8960120423e53ebeef1c28a523159
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-01-21 14:51:37 -0800

    added react-bootstrap

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+export default function Nav() {
+
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>
+        </div>
+    )
+}
