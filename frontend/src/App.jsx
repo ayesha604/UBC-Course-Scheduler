e3bef4c69dcb53e71d3862e4dae7e7e8232be30d
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-01-21 16:07:59 -0800

    added fetch course list on page load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.scss'
 import Nav from './components/Nav'
 
+const prefetchCourseURL = 'http://127.0.0.1:5000/valid_courses'
+
 function App() {
 
+  useEffect(() => {
+    async function prefetch() {
+      try {
+        let response = (await fetch(prefetchCourseURL))
+        console.log(await response.text())
+      } catch(e) {
+        console.error("Network request failed")
+      }
+    }
+    prefetch()
+  }, [])
+
+  
   return (
     <div className="App">
       <Nav />
