7cea6376798894cca3b24faa814d1707a63b0699
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-01-22 02:21:02 -0800

    load coarses from names

--- backend/objects/CoursesLoader.py
+++ backend/objects/CoursesLoader.py
@@ -1,4 +1,40 @@
 import json
+from dacite import from_dict
+
+from backend.objects.Course import Course
+from backend.objects.NewSection import Section
+
+
+def course_from_dict(course_dict: dict) -> Course:
+    name = course_dict['name']
+    sections = []
+    for section in course_dict['sections']:
+        sections.append(section_from_dict(section))
+    requirements = course_dict['requirements']
+    creds = course_dict['credits']
+    return Course(name, sections, requirements, creds)
+
+
+def section_from_dict(section_dict: dict) -> Section:
+    # why
+    status = section_dict['status']
+    name = section_dict['name']
+    activity = section_dict['activity']
+    term = section_dict['term']
+    mode = section_dict['mode']
+    times = section_dict['times']
+    location = section_dict['location']
+    professor = section_dict['professor']
+    dependencies = section_dict['dependencies']
+
+    if dependencies is not None:
+        dep_list = []
+        for dep_dict in dependencies:
+            dep = section_from_dict(dep_dict)
+            dep_list.append(dep)
+
+    return Section(status, name, activity, term, mode, times,
+                   location, professor, dependencies)
 
 
 class CoursesLoader:
@@ -11,4 +47,15 @@ class CoursesLoader:
             self.courses = json.load(f)
 
     def get_course_names(self) -> list[str]:
+        """Return all loaded course names"""
         return list(self.courses.keys())
+
+    def get_courses_from_names(self, names: list[str]) -> list[Course]:
+        """Get a list of Course objects from names"""
+        courses = []
+        for name in names:
+            if name in self.courses.keys():
+                courses.append(course_from_dict(self.courses[name]))
+            else:
+                raise KeyError(f'Could not find course {name}')
+        return courses
--- backend/tests/test_loader.py
+++ backend/tests/test_loader.py
@@ -6,7 +6,9 @@ SAVED_COURSES_PATH = './backend/saved_json/saved_courses.json'
 def main():
     loader = CoursesLoader()
     loader.load(SAVED_COURSES_PATH)
-    print(loader.get_course_names())
+    courses = loader.get_courses_from_names(['CPSC 110', 'MATH 100'])
+    for course in courses:
+        print(course)
 
 
 if __name__ == '__main__':
